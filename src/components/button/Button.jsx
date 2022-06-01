798e5cb4a4faa5ea79f9af8ec8180b7eaeeb2740
5arBarisic <93656929+5arBarisic@users.noreply.github.com>
2022-06-01 16:50:49 +0200

    Updated Button and authentication

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import './Button.css';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
 
 export function Button() {
+
+  
+
+  const { user, logout } = UserAuth();
+  const navigate = useNavigate();
+
+
+  async function handleLogout() {
+    try {
+      await logout();
+      navigate('/sign_in');
+      console.log('You are logged out');
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+ 
+  if((user && user.email)){
   return (
-    <Link to='sign_in'>
-      <button className='btn'>Prijavi se</button>
-    </Link>
+    <NavLink to='sign_in'>
+      <button className='btn' onClick={ handleLogout }> Log Out</button>
+    </NavLink>
+  );
+  }
+   return (
+    <NavLink to='sign_in'>
+      <button className='btn'> Log In</button>
+    </NavLink>
   );
 }
