0730bc09c051ce897ed1ae98469d24bf726d9034
KristinaBeslic <93668625+KristinaBeslic@users.noreply.github.com>
2022-09-05 18:18:17 +0200

    Updated Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -11,10 +11,17 @@ export default function Home() {
   return (
     <>
       <section className='home'>
-        <div className='beers-div'>
-          <div className='label'>
-          <h2>Piva</h2>
-        </div>
+          <div className='aboutCraft'>
+            <p className='introduction'>Dobrodošli na stranicu o hrvatskom tekućem zlatu</p>
+            <p className='homepageParagraph'>Prošetat ćemo se kroz hrvatske pivovare te upoznati različita craft piva...</p>
+            <p className='whatIsCraft'>Što je to craft pivo?</p>
+            <p className='whatIsCraft2'>Pivo proizvedeno na kreativan način, bez korištenja jeftinih sirovina kako bi se uštedjelo u proizvodnji, 
+                a proizvedeno od strane male nezavisne pivovare!</p>
+          </div>
+          <div className='beers-div'>
+            <div className='label'>
+            <h2>Piva</h2>
+          </div>
           <div className='beers-slider'>
             <Beers
               data={beers}
